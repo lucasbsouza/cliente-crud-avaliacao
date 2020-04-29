1c685d7fed2fb2e20d544b1b053bfc0ff089bfa5
Lucas <@lucasbsouza>
2020-04-29 20:29:03 -0300

    Teste solicitado

--- src/main/java/br/com/tokiomarine/controller/ClienteController.java
+++ src/main/java/br/com/tokiomarine/controller/ClienteController.java
@@ -1,23 +1,62 @@
 package br.com.tokiomarine.controller;
 
+import javax.validation.constraints.NotEmpty;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.tokiomarine.domain.Cliente;
 import br.com.tokiomarine.repository.ClienteRepository;
+import br.com.tokiomarine.util.Validate;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 @RestController
 @RequestMapping("clientes")
+@Api("API Clientes")
 public class ClienteController {
-	
+
 	@Autowired
 	private ClienteRepository clienteRepository;
-	
-	@GetMapping()
-	public Iterable<Cliente> listarClientes() {
-		return clienteRepository.findAll();
+
+	@GetMapping
+	@ApiOperation(value = "Listagem de Clientes")
+	public ResponseEntity<Iterable<Cliente>> listarClientes() {
+		return ResponseEntity.ok(clienteRepository.findAll());
 	}
-	
+
+	@DeleteMapping("/{id}")
+	@ApiOperation(value = "Remoção de Clientes")
+	public ResponseEntity<Boolean> deletarCliente(@PathVariable Long id) {
+		clienteRepository.deleteById(id);
+		return ResponseEntity.ok(true);
+	}
+
+	@PostMapping("/{id}/email/{email}/nome/{nome}")
+	@ApiOperation(value = "Alteração de cliente")
+	public ResponseEntity<Object> alterarCliente(@NotEmpty(message = "Campo id é obrigatório") 
+												 @PathVariable("id") Long id,
+												 @PathVariable("email") String email,
+												 @PathVariable("nome") String nome) {
+		
+		Cliente bdCli;
+		try {
+			Validate.isValid(email, "email");
+			Validate.isValid(nome, "nome");
+			bdCli = clienteRepository.findById(id).orElseThrow(() -> new RuntimeException("Cliente não encontrado"));
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+		}
+		
+		Cliente save = Cliente.builder().nome(nome).email(email).id(bdCli.getId()).build();
+		clienteRepository.save(save);
+		return ResponseEntity.ok(save);
+	}
 }
--- src/main/java/br/com/tokiomarine/domain/Cliente.java
+++ src/main/java/br/com/tokiomarine/domain/Cliente.java
@@ -1,16 +1,25 @@
 package br.com.tokiomarine.domain;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 
-import lombok.Data;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
-@Data
+@Getter
+@Setter
+@Builder
 public class Cliente {
 
 	@Id
@@ -18,12 +27,33 @@ public class Cliente {
 	private Long id;
 
 	@Column(nullable = false)
-	@NotEmpty
+	@NotEmpty(message = "Preencha o Nome")
 	private String nome;
 
 	@Column(nullable = false)
 	@NotEmpty
-	@Email
+	@NotEmpty(message = "Preencha o Email")
 	private String email;
+	
+	@OneToMany(mappedBy = "cliente", fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
+	private List<Endereco> enderecos;
+	
+	public Cliente() {}
+
+	public Cliente(Long id, @NotEmpty String nome, @NotEmpty @Email String email, List<Endereco> enderecos) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.enderecos = enderecos;
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente [id=" + id + ", nome=" + nome + ", email=" + email + ", enderecos=" + enderecos + "]";
+	}
+	
+	
+	
 
 }
